f79b3244f943c83ebb9c6f940a857eafff6e8d44
RichardMilos233 <richardmilos233@gmail.com>
2026-02-05 15:56:24 +0800

    232. Implement Queue using Stacks

---
+++ 232.py
@@ -0,0 +1,58 @@
+# Implement a first in first out (FIFO) queue using only two stacks. 
+# The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+# Implement the MyQueue class:
+
+# void push(int x) Pushes element x to the back of the queue.
+# int pop() Removes the element from the front of the queue and returns it.
+# int peek() Returns the element at the front of the queue.
+# boolean empty() Returns true if the queue is empty, false otherwise.
+
+# Notes:
+
+# You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+class MyQueue:
+
+  def __init__(self):
+    self.s = []
+    self.rs = []
+      
+
+  def push(self, x: int) -> None:
+    self.s.append(x)
+      
+
+  def pop(self) -> int:
+    while self.s:
+      x = self.s.pop()
+      self.rs.append(x)
+    out = self.rs.pop()
+    while self.rs:
+      x = self.rs.pop()
+      self.s.append(x)
+    return out
+      
+
+  def peek(self) -> int:
+    while self.s:
+      x = self.s.pop()
+      self.rs.append(x)
+    out = self.rs[-1]
+    while self.rs:
+      x = self.rs.pop()
+      self.s.append(x)
+    return out
+      
+
+  def empty(self) -> bool:
+    return not self.s
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
